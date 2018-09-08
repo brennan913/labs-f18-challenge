ad1ea168026ec73bd1e72631cd4eafdf9769639e
BXM <brennan913@gmail.com>
2018-09-07 23:43:10 -0400

    working version

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
+import requests
+#import json
 
 app = Flask(__name__)
 
+base_url = 'http://pokeapi.co/api/v2/pokemon/'
+
 # show name method
-@app.route('/pokemon/<int:p_dex_num>')
+@app.route('/pokemon/<int:pdex>')
+def get_name(pdex):
+    url = base_url + str(pdex) + '/'
+    request = requests.get(url)
+    name = request.json()['name']
+    return 'The pokemon with id %d is %s' % (pdex, name)
 
 # show pokedex number method
-@app.route('/pokemon/<id>')
+@app.route('/pokemon/<name>')
+def get_pdex(name):
+    url = base_url + name + '/'
+    request = requests.get(url)
+    pdex = request.json()['id']
+    return '%s has id %d' % (name, pdex)
 
 # main method
 @app.route('/', methods=['GET'])
