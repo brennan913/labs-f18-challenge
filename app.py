5be055e0fc37c9c31f440a3f4e3789087f4e79ae
BXM <brennan913@gmail.com>
2018-09-07 22:58:29 -0400

    skeleton code

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# show name method
+@app.route('/pokemon/<int:p_dex_num>')
 
+# show pokedex number method
+@app.route('/pokemon/<id>')
+
+# main method
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
